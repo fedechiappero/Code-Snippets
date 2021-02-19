b0e4ea4a5766dc1f05e984a710a0a0f58e33ed48
Federico Chiappero <fedechiappero@users.noreply.github.com>
2021-02-19 15:16:33 -0300

    VB - Check if recordset item is null

--- VisualBasic.vb
+++ VisualBasic.vb
@@ -4,3 +4,6 @@ Dim strArr() As String
 str = "14/02/2020"
 strArr = str.Split("/")
 strArr(0) & strArr(1) & strArr(2)
+
+' Check if recordset item is null
+rs.Fields.Item("field").Value Is DBNull.Value
